ba7408a2dc5374523fb7270e5fd0f6267aa8ad0d
brantwynn <brant.wynn@acquia.com>
2014-01-28 23:17:19 -0500

    update df_admin screen and add screenshots for scenarios

---
+++ modules/df/df_admin/df-admin-page.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file df-admin-page.tpl.php
+ */
+?>
+
+<div class="dfs-wrapper">
+  <?php foreach ($scenarios as $scenario): ?>
+    <div class="dfs-item">
+      <?php if (isset($scenario['screenshot'])): ?>
+        <div class="dfs-screenshot"><img src="<?php echo $scenario['screenshot']; ?>"></div>
+      <?php endif; ?>
+      <h3 class="dfs-name"><?php echo $scenario['name']; ?></h3>
+      <div class="dfs-description"><?php echo $scenario['description']; ?></div>
+      <div class="dfs-link"><?php echo $scenario['link']; ?></div>
+    </div>
+  <?php endforeach; ?>
+</div>
