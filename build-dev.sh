fb58e008d22eee370f4ab73678b4875475a6eb62
brantwynn <brant.wynn@acquia.com>
2014-02-06 17:52:01 -0600

    add build-dev.sh & remove working-copy directive from build.sh

--- build-dev.sh
+++ build-dev.sh
@@ -3,6 +3,11 @@
 MAKEFILE='build-df.make'
 TARGET=$1
 CALLPATH=`dirname $0`
+# Remove target directory if it exists.
+if [[ -r $TARGET ]]; then
+  rm -rf $TARGET
+fi
+# Macro the distro name.
 echo '      ____________________________'
 echo '     _/\/\/\/\/\____/\/\/\/\/\/\_'
 echo '    _/\/\____/\/\__/\/\_________'
@@ -10,4 +15,5 @@ echo '   _/\/\____/\/\__/\/\/\/\/\___'
 echo '  _/\/\____/\/\__/\/\_________'
 echo ' _/\/\/\/\/\____/\/\_________'
 echo '____________________________'
-drush make --concurrency=5 $CALLPATH/$MAKEFILE $TARGET
+# Run drush make using --working-copy directive
+drush make --working-copy --concurrency=5 $CALLPATH/$MAKEFILE $TARGET
