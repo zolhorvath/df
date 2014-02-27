d26f090150b408101296cc6f0c82f4a34263d1b0
brantwynn <brant.wynn@acquia.com>
2014-02-27 13:56:25 -0600

    updated styles in df_admin page

--- modules/df/df_admin/df-admin-page.tpl.php
+++ modules/df/df_admin/df-admin-page.tpl.php
@@ -10,7 +10,7 @@
       <?php if (isset($scenario['screenshot'])): ?>
         <div class="dfs-screenshot"><img src="<?php echo $scenario['screenshot']; ?>"></div>
       <?php endif; ?>
-      <h3 class="dfs-name"><?php echo $scenario['name']; ?></h3>
+      <h4 class="dfs-name"><?php echo $scenario['name']; ?></h4>
       <div class="dfs-description"><?php echo $scenario['description']; ?></div>
       <div class="dfs-link"><?php echo $scenario['link']; ?></div>
     </div>
