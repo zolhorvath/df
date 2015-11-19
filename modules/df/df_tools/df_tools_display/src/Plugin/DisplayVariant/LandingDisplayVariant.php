e99f415c95f6c3ea5ed603cf4fa6199e4ecaaf57
Samuel Mortenson <samuel.mortenson@acquia.com>
2015-11-19 09:47:42 -0800

    Fixed bug where our wrapping class in LandingDisplayVariant was never applied.

--- modules/df/df_tools/df_tools_display/src/Plugin/DisplayVariant/LandingDisplayVariant.php
+++ modules/df/df_tools/df_tools_display/src/Plugin/DisplayVariant/LandingDisplayVariant.php
@@ -8,7 +8,8 @@
 namespace Drupal\df_tools_display\Plugin\DisplayVariant;
 
 use Drupal\Component\Utility\Html;
-use Drupal\page_manager\Plugin\DisplayVariant\BlockDisplayVariant;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\page_manager\Plugin\DisplayVariant\PageBlockDisplayVariant;
 
 /**
  * Provides a display variant that simply contains blocks.
@@ -18,10 +19,10 @@ use Drupal\page_manager\Plugin\DisplayVariant\BlockDisplayVariant;
  *   admin_label = @Translation("Landing page")
  * )
  */
-class LandingDisplayVariant extends BlockDisplayVariant {
+class LandingDisplayVariant extends PageBlockDisplayVariant {
 
   /**
-   * @see \Drupal\page_manager\Plugin\BlockVariantInterface::getRegionNames()
+   * @see \Drupal\page_manager\Plugin\PageBlockVariantInterface::getRegionNames()
    */
   public function getRegionNames() {
     return [
@@ -37,15 +38,59 @@ class LandingDisplayVariant extends BlockDisplayVariant {
   /**
    * {@inheritdoc}
    */
-  public function build() {
-    $build = parent::build();
+  public function buildRegions(array $build) {
+    // Get page_class configuration for our display variant.
+    $class = Html::getClass($this->configuration['page_class']);
+    if ($class == '') {
+      $class = 'default';
+    }
 
-    // Add a class that represents the current page to our display variant
-    $id = Html::getClass($this->executable->getPage()->id());
-    $build['regions']['#prefix'] = '<div class="landing-display-' . $id . '">';
-    $build['regions']['#suffix'] = '</div>';
+    // Append the prefix before buildRegions.
+    $build['landing-top']['#prefix'] = '<div class="landing-display-' . $class . '">';
+
+    // Build the regions via PageBlockDisplayVariant::buildRegions.
+    $build = parent::buildRegions($build);
+
+    // Add suffix for wrapper after buildRegions.
+    $build['landing-bottom']['#suffix'] = '</div>';
 
     return $build;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    // Allow to configure the variant class, even when adding a new display.
+    $form['page_class'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Page class'),
+        '#description' => $this->t('Configure the wrapper class that will be used for this display.'),
+        '#default_value' => $this->configuration['page_class'] ?: '',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    if ($form_state->hasValue('page_class')) {
+      $this->configuration['page_class'] = $form_state->getValue('page_class');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+        'page_class' => '',
+    ];
+  }
 }
