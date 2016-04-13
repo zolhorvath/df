73c01be570b15494337b86e8eb82640ee062959f
Brant Wynn <brant.wynn@acquia.com>
2016-04-13 13:03:07 -0500

    Tie specific symfony dependencies, remove hacks.

--- .drupal-ti/runners/df-behat/script.sh
+++ .drupal-ti/runners/df-behat/script.sh
@@ -19,13 +19,6 @@ mv -f "$TRAVIS_BUILD_DIR"/behat.travis.yml.dist .
 # We need to create a behat.yml file from behat.yml.dist.
 drupal_ti_replace_behat_vars
 
-#------------------------------------------------
-# Temp fix for Symfony, replace our composer.json
-rm -rf composer.json
-wget https://raw.githubusercontent.com/acquia/df/a86afe2270d3c81ccd44b06e910f39bffe6ae067/tests/composer.json
-composer update
-#-----------------------------------------------
-
 # And run the tests.
 ARGS=( $DRUPAL_TI_BEHAT_ARGS )
 if [[ "$SCENARIO" != none ]]; then ./bin/behat --tags=df; fi
