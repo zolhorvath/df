27aa0f6d5dd7858859994c8021637be923eed920
Brian Wald <briancwald@gmail.com>
2016-05-09 12:07:05 -0400

    add generic function for foundation tabs layout

--- themes/dfs_base/js/dfs_base.js
+++ themes/dfs_base/js/dfs_base.js
@@ -1,17 +1,44 @@
 /**
  * @file
- * Placeholder file for custom sub-theme behaviors.
+ * Generic functions for all our Foundation Scenarios
  *
  */
 (function ($, Drupal) {
 
-  /**
-   * Use this behavior as a template for custom Javascript.
-   */
-  Drupal.behaviors.exampleBehavior = {
-    attach: function (context, settings) {
-      //alert("I'm alive!");
+  // Add the active class to the first available Foundation tab.
+  //set active class for tabs for whatever is returned by views
+  $(document).ready(function() {
+    function openTab() {
+      var FoundationTabA = $('.tabs li a');
+      var FoundationFirstTabA = $('.tabs').find('li a').first();
+      var FoundationFirstTabPanel = $('.tabs-panel').first();
+      var origHash = window.location.hash;
+      if (origHash) {
+        var TabURL = '#' + window.location.hash.substr(1);
+      };
+      if (typeof TabURL !== 'undefined') {
+        FoundationTabA.each(function(e) {
+          var hash = $(this).attr('href');
+          if (hash == TabURL) {
+            $(this).click();
+            $('html, body').animate({
+              scrollTop: $('.tabs-title a').offset().top
+            }, 2000);
+            return false;
+          }
+        });
+      } else {
+        // open the first tab by default
+        FoundationFirstTabA.click();
+        FoundationFirstTabPanel.addClass('is-active');
+      }
     }
-  };
+    $(window).bind('hashchange', function() {
+      openTab();
+    });
+
+    openTab();
+  });
+
 
 })(jQuery, Drupal);
