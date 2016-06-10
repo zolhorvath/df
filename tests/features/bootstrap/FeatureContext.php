a038b44b57dd1938a4c38fc0e53f08f7e6d71512
Devin Carlson <devin@devincarlson.ca>
2016-06-10 05:09:21 -0400

    Reworked test infrastructure.

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,35 +1,18 @@
 <?php
+
 /**
  * @file
- * The main Behat context.
+ * Contains \FeatureContext.
  */
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 
 /**
- * Define application features from the specific context.
+ * Defines application features from the specific context.
  */
 class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
-  /**
-   * Initializes context.
-   * Every scenario gets its own context object.
-   *
-   * @param array $parameters
-   *   Context parameters (set them in behat.yml)
-   */
-  public function __construct() {
-    // Initialize your context here
-  }
-
-//
-// Place your definition and hook methods here:
-//
-//  /**
-//   * @Given I have done something with :stuff
-//   */
-//  public function iHaveDoneSomethingWith($stuff) {
-//    doSomethingWith($stuff);
-//  }
-//
+  // DFSubContext is automatically loaded by the Drupal Behat Extension.
+  // @see df.behat.inc.
+  // @see http://behat-drupal-extension.readthedocs.org/en/3.1/subcontexts.html#for-contributors
 }
