49497450d5f889c8c2aa5f2075573b98cbd7e482
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-07-28 16:27:46 -0700

    Updated Map Block base migration as a follow up to recent refactors.

--- modules/df/df_tools/df_tools_map/src/Plugin/migrate/source/MapBlock.php
+++ modules/df/df_tools/df_tools_map/src/Plugin/migrate/source/MapBlock.php
@@ -15,12 +15,39 @@ use Drupal\migrate_source_csv\Plugin\migrate\source\CSV;
 class MapBlock extends CSV {
 
   public function prepareRow(Row $row) {
-    if (($lat = $row->getSourceProperty('Lat')) && ($lng = $row->getSourceProperty('Long'))) {
-      $location = array(
-        'lat' => $lat,
-        'lng' => $lng
-      );
-      $row->setSourceProperty('Location', $location);
+    // Prefix the state per the normal Address schema.
+    if ($state = $row->getSourceProperty('Address State')) {
+      $row->setSourceProperty('Address State', 'US-' . $state);
+    }
+
+    // Use a cached geolocation if possible.
+    if ($geolocation = $row->getSourceProperty('Geolocation')) {
+      list($lat, $lon) = explode(',', $geolocation);
+      $point = [$lon, $lat];
+      $row->setSourceProperty('Geofield', \Drupal::service('geofield.wkt_generator')->WktBuildPoint($point));
+    }
+    else {
+      // Manually geocode from source information.
+      $address = $row->getSourceProperty('Address Line 1');
+      $address2 = $row->getSourceProperty('Address Line 2');
+      $city = $row->getSourceProperty('Address City');
+      $zip = $row->getSourceProperty('Address Zip');
+
+      if ($address && $city && $zip && $state) {
+        $address2 = $address2 ? $address2 . "\n" : '';
+        $address_string = "$address\n$address2$city, $state $zip\nUS";
+        if ($collection = \Drupal::service('geocoder')->geocode($address_string, ['googlemaps'])) {
+          // Set our value in a similar way to Geofield's LatLon Widget.
+          // @see \Drupal\geofield\Plugin\Field\FieldWidget\GeofieldLatLonWidget::massageFormValues()
+          $coordinates = $collection->first()->getCoordinates();
+          $point = [
+            $coordinates->getLongitude(),
+            $coordinates->getLatitude()
+          ];
+          $row->setSourceProperty('Geofield', \Drupal::service('geofield.wkt_generator')
+            ->WktBuildPoint($point));
+        }
+      }
     }
   }
 
