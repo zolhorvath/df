647d47012102e8e0ee55c3adcaceb10553b0f132
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-11-10 10:05:40 -0800

    Removed non-icon fonts and cleaned up code style a bit.

--- themes/dfs_base/js/dfs_base.js
+++ themes/dfs_base/js/dfs_base.js
@@ -3,36 +3,36 @@
  * Generic functions for all our Foundation Scenarios
  *
  */
-(function($, Drupal) {
 
-  //FOUC fix for menu
-  function showMenu() {
+(function ($, Drupal) {
+
+  // FOUC fix for menu.
+  function showMenu () {
     $('#top-bar-sticky-container').show();
   }
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     if (Foundation.MediaQuery.atLeast('medium')) {
       showMenu();
     }
 
-    $(window).on('changed.zf.mediaquery', function(event, medium, small) {
+    $(window).on('changed.zf.mediaquery', function (event, medium, small) {
       showMenu();
     });
   });
 
   // Add the active class to the first available Foundation tab.
-  //set active class for tabs for whatever is returned by views
-  $(document).ready(function() {
-    function openTab() {
+  $(document).ready(function () {
+    function openTab () {
       var FoundationTabA = $('.tabs li a');
       var FoundationFirstTabA = $('.tabs').find('li a').first();
       var FoundationFirstTabPanel = $('.tabs-panel').first();
       var origHash = window.location.hash;
       if (origHash) {
         var TabURL = '#' + window.location.hash.substr(1);
-      };
+      }
       if (typeof TabURL !== 'undefined') {
-        FoundationTabA.each(function(e) {
+        FoundationTabA.each(function (e) {
           var hash = $(this).attr('href');
           if (hash == TabURL) {
             $(this).click();
@@ -43,36 +43,32 @@
           }
         });
       } else {
-        // open the first tab by default
+        // Open the first tab by default.
         FoundationFirstTabA.click();
         FoundationFirstTabPanel.addClass('is-active');
       }
     }
-    $(window).bind('hashchange', function() {
+
+    $(window).bind('hashchange', function () {
       openTab();
     });
 
     openTab();
   });
 
-  //after 5 seconds, hide the notifcations
-  function removeWhiteSpace() {
+  // After 5 seconds, hide notifications.
+  function removeWhiteSpace () {
     $('.region-highlighted').removeClass('callout');
   }
-  setTimeout(function() {
-    elem = $(".zurb-foundation-callout")
+
+  setTimeout(function () {
+    elem = $('.zurb-foundation-callout');
     Foundation.Motion.animateOut(elem, 'hinge-out-from-top', removeWhiteSpace);
   }, 5000);
 
-  //If workbench moderation form is on, move it to mm-panel. 
-  //This is required as mmmenu cannot accept form elements, so we do it after dom load.
-  $(document).ready(function() {
-    $('.workbench-moderation-entity-moderation-form').appendTo('.mm-panel');
-  });
-
-  //when a comment link is clicked, smooth scroll
+  // When a comment link is clicked, smooth scroll.
   var $root = $('html, body');
-  $('.author-text-area a').click(function() {
+  $('.author-text-area a').click(function () {
     $root.animate({
       scrollTop: $($.attr(this, 'href')).offset().top - 150
     }, 1000);
@@ -89,7 +85,7 @@
           $modal.dialog({
             width: '100%',
             height: $(window).height(),
-            close: function() {
+            close: function () {
               $('body').removeClass('full-width-modal-open');
             }
           });
@@ -101,8 +97,8 @@
     }
   });
 
-  Drupal.behaviors.formFilled = {
-    attach: function(context, settings) {
+  Drupal.behaviors.dfs_base.formFilled = {
+    attach: function (context, settings) {
       var formID = $('.contact-form');
       var checkbox = $('.form-checkbox');
       var radio = $('.form-radio');
@@ -110,11 +106,10 @@
       var textItem = $('.form-text');
       var select = $('.form-select');
 
-      function isChecked() {
-
-        $(this).each(function() {
-          $(this).prop('checked') ? $(this).next('label').addClass('item-selected') : $(this).next('label').removeClass('item-selected');
-
+      function isChecked () {
+        $(this).each(function () {
+          var $label = $(this).next('label');
+          $(this).prop('checked') ? $label.addClass('item-selected') : $label.removeClass('item-selected');
         });
         var limit = 3;
         if ($(this).parent().siblings().children(':checked').length > limit) {
@@ -124,18 +119,16 @@
           $(this).closest('fieldset').find('legend').removeClass('alert');
           $(this).closest('fieldset').find('.description').removeClass('alert');
         }
-
       }
 
-      function isFilled() {
-        $(this).each(function() {
-          ($(this).val().trim().length > 0) ? $(this).closest('.form-item').find('label').addClass('item-selected'): $(this).closest('.form-item').find('label').removeClass('item-selected')
-
+      function isFilled () {
+        $(this).each(function () {
+          var $label = $(this).closest('.form-item').find('label');
+          ($(this).val().trim().length > 0) ? $label.addClass('item-selected') : $label.removeClass('item-selected');
         })
-
       }
 
-      $(document).change(function() {
+      $(document).change(function () {
         if (formID.length) {
           isChecked.call(checkbox);
           isChecked.call(radio);
@@ -143,10 +136,9 @@
           isFilled.call(textItem);
           isFilled.call(select);
         }
-
       });
 
-      $(document).ready(function() {
+      $(document).ready(function () {
         if (formID.length) {
           isChecked.call(checkbox);
           isChecked.call(radio);
@@ -154,66 +146,99 @@
           isFilled.call(textItem);
           isFilled.call(select);
         }
-
       });
     }
   };
 
-  //another foundation bug on resize. When the window resizes, the hidden dropdown page causes the page to expand beyond window size.
-  //it only uses visibilitiy hidden, but we need display none
-  Drupal.behaviors.FoundationUser = {
-    attach: function(context, settings) {
+  // Another foundation bug on resize. When the window resizes, the hidden
+  // dropdown page causes the page to expand beyond window size.
+  // It only uses visibility hidden, but we need display none.
+  Drupal.behaviors.dfs_base.FoundationUser = {
+    attach: function (context, settings) {
       var dropDownButton = $('.login-dropdown-button');
       var languageDownButton = $('.language-dropdown-button');
       var dropDownPane = $('.dropdown-pane');
       var languagePane = $('.language-pane');
-      //add class on page load
-      $(document).ready(function() {
+      // Add class on page load.
+      $(document).ready(function () {
         dropDownPane.addClass('display-none');
         languagePane.addClass('display-none');
-      }),
-        //toggle class on click
-        dropDownButton.on('click', function() {
-          dropDownPane.removeClass('display-none');
-        });
-      languageDownButton.on('click', function() {
+      });
+      // Toggle class on click.
+      dropDownButton.on('click', function () {
+        dropDownPane.removeClass('display-none');
+      });
+      languageDownButton.on('click', function () {
         languagePane.removeClass('display-none');
       });
     }
   };
 
-  //temp fix until this is resolved: http://foundation.zurb.com/forum/posts/38056-responsive-navigation-problem
-  Drupal.behaviors.FoundationMenu = {
-    attach: function(context, settings) {
+  // Temp fix until this is resolved: http://foundation.zurb.com/forum/posts/38056-responsive-navigation-problem
+  Drupal.behaviors.dfs_base.FoundationMenu = {
+    attach: function (context, settings) {
       var Icon = $('button.menu-icon');
       var topbar = $('.top-bar');
       var topbarDrop = $('.topbar-toplevel');
-      Icon.click(function(t) {
+      Icon.click(function (t) {
         topbarDrop.toggleClass('vertical');
       });
 
-      $(window).on('changed.zf.mediaquery', function(event, medium, small) {
+      $(window).on('changed.zf.mediaquery', function (event, medium, small) {
         if ((topbarDrop).hasClass('vertical')) {
           topbarDrop.toggleClass('vertical');
         }
       });
-
     }
   };
 
-  //remove sticky nav when Quickedit is turned on as it causes all kinds of complications
-  Drupal.behaviors.qe = {
-    attach: function(context, settings) {
-      $(document).on('DOMNodeInserted', function(e) {
+  // Remove sticky nav when Quick Edit is turned on as it causes all kinds of
+  // complications.
+  Drupal.behaviors.dfs_base.QuickEdit = {
+    attach: function (context, settings) {
+      $(document).on('DOMNodeInserted', function (e) {
         if (e.target.id == 'quickedit-entity-toolbar') {
           $('body').addClass('quickedit-on');
           $('.top-bar-wrapper .sticky').removeAttr('data-sticky').removeClass('sticky');
         }
       });
-      $(document).on('DOMNodeRemoved', function(e) {
+      $(document).on('DOMNodeRemoved', function (e) {
         if (e.target.id == 'quickedit-entity-toolbar') {
           $('body').removeClass('quickedit-on');
           $('.top-bar-wrapper .sticky-container > div').addClass('sticky').attr('data-sticky');
+        }
+      });
+    }
+  };
+
+  // Sometimes we get weird issues with images not loading due to image styles,
+  // so at least provide a fallback.
+  Drupal.behaviors.dfs_base.imgError = {
+    attach: function (context, settings) {
+      $('img').one('error', function () {
+        var image = this;
+        var thisSrc = this.src;
+        var matchSitesDefault = thisSrc.match(/\/sites\/(.*)\/files\/styles\//);
+        var width = $(image).attr('width');
+        var height = $(image).attr('height');
+
+        if (matchSitesDefault) {
+          console.log('ATTENTION: The image on this page may be broken because a Dev Desktop bug, please go to /admin/appearance/ and save the form');
+        }
+        // If both elements are undefined, just hide them.
+        if (!height && !width) {
+          $(image).hide();
+          console.log('404 broken image hidden with this path:' + thisSrc);
+        } else {
+          // Take attr and move it to css height/width. then display the.
+          $(image).wrap("<span class='broken-image-wrap'></span>");
+          $(image).parent().css({
+            'height': height,
+            'width': width,
+          });
+          // Still hide the image to remove browser default icon, because we
+          // are now using background image.
+          $(image).hide();
         }
       });
     }
