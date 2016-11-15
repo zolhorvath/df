a9fe2e899cc3212091403b277dd892fb8572054c
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-11-14 17:09:30 -0800

    Added an example migration for Media Image entities.

---
+++ modules/df/df_tools/df_tools_media/src/Plugin/migrate/source/MediaImage.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\import\Plugin\migrate\source\ImageFile.
+ */
+
+namespace Drupal\df_tools_media\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\migrate_source_csv\Plugin\migrate\source\CSV;
+
+/**
+ * Source for the Media Image csv.
+ *
+ * @MigrateSource(
+ *   id = "media_image"
+ * )
+ */
+class MediaImage extends CSV {
+
+  public function prepareRow(Row $row) {
+    if ($image = $row->getSourceProperty('Image')) {
+      $files = \Drupal::entityTypeManager()
+        ->getStorage('file')->loadByProperties(['filename' => $image]);
+
+      if (empty($files)) {
+        $base_path = dirname($this->configuration['path']) . '/images/';
+        $path = $base_path . $image;
+        $data = file_get_contents($path);
+        $uri = file_build_uri($image);
+        $file = file_save_data($data, $uri);
+        echo "\nSAVED NEW FILE $image\n";
+      }
+      else {
+        echo "\nFOUND FILE $image\n";
+        $file = reset($files);
+      }
+
+      $row->setSourceProperty('File', $file);
+    }
+  }
+
+}
