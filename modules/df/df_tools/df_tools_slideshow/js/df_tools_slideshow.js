31baea67de3a2159463025e67c03d346d6d4fb7a
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-11-18 14:25:08 -0800

    Added the ability to use a view mode as the pager in EntityReferenceSlideshowFormatter.

--- modules/df/df_tools/df_tools_slideshow/js/df_tools_slideshow.js
+++ modules/df/df_tools/df_tools_slideshow/js/df_tools_slideshow.js
@@ -9,7 +9,16 @@
 
   Drupal.behaviors.DFToolsSlideshow = {
     attach: function (context, settings) {
-      $('.df-tools-slideshow > .field-items').not('.slick-initialized').slick(settings.DFToolsSlideshow.slick);
+      // @todo Support multiple slideshows on one page with different settings.
+      var $slideshow = $('.df-tools-slideshow > .field-items').not('.slick-initialized');
+      if (settings.DFToolsSlideshow.slick.customPaging === true) {
+        settings.DFToolsSlideshow.slick.customPaging = function(slider, i) {
+          var $slide = $(slider.$slides[i]);
+          return $('<a href="javascript:;" class="df-tools-slideshow-custom-pager"></a>').append($slide.find('.df-tools-slideshow-pager-element').clone());
+        };
+        $slideshow.addClass('df-tools-slideshow-uses-custom-pager');
+      }
+      $slideshow.not('.slick-initialized').slick(settings.DFToolsSlideshow.slick);
     }
   };
 
