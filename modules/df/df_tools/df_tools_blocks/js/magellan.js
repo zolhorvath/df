d94389f566b75b5ec59c2dce74aa9621691f4bbc
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-12-05 14:20:11 -0800

    Worked around Foundation bugs in the Magellan block's JS.

--- modules/df/df_tools/df_tools_blocks/js/magellan.js
+++ modules/df/df_tools/df_tools_blocks/js/magellan.js
@@ -11,6 +11,11 @@
   Drupal.behaviors.DFToolsBlocksMagellan = {
     attach: function (context) {
       $('.df-tools-magellan-block', context).once('magellan').each(function () {
+        if (Drupal.panels_ipe && Drupal.panels_ipe.app && Drupal.panels_ipe.app.get('active')) {
+          $(this).find('[data-sticky]').attr('style', '');
+          $(this).find('[data-sticky]').removeClass('sticky');
+          return;
+        }
         var $blocks_after = $(this).parents().eq(1).nextAll();
         var $menu = $(this).find('.menu');
         $menu.empty();
@@ -19,6 +24,7 @@
             if (!this.hasAttribute('id')) {
               $(this).uniqueId();
             }
+            $(this).attr('data-magellan-target', $(this).attr('id'));
             var $anchor = $('<a></a>');
             $anchor.attr('href', '#' + $(this).attr('id'));
             $anchor.text($(this).text());
@@ -33,6 +39,15 @@
         $(this).foundation();
         $(this).find('a').on('click', function () {
           $('[data-magellan]').data('zfPlugin').options.barOffset = $('#toolbar-bar:visible').outerHeight() + $('#toolbar-item-administration-tray:visible').outerHeight() + $(this).outerHeight() - 10;
+        });
+      });
+
+      // Temporary fix until https://github.com/zurb/foundation-sites/issues/8016 is closed.
+      $(window).once('temporary-magellan-fix').on('scroll hashchange', function () {
+        $('[data-magellan]').each(function () {
+          if ($(this).data('zfPlugin')) {
+            $(this).foundation('reflow');
+          }
         });
       });
     }
