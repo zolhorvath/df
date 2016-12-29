085fbb5535b1d7a697d2a373d409fe9b82ec9d26
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-12-29 12:17:34 -0800

    Added behaviors to df_tools_frontend to improve the IPE flow.

---
+++ modules/df/df_tools/df_tools_frontend/js/panels_ipe.js
@@ -0,0 +1,23 @@
+/**
+ * @file panels_ipe.js
+ */
+(function ($, Drupal, Backbone) {
+
+  "use strict";
+
+  /**
+   * Reacts to IPE states to better direct demo flow.
+   */
+  Backbone.on('PanelsIPEInitialized', function () {
+    Drupal.panels_ipe.app.get('layout').on('sync', function () {
+      location.reload();
+    }, 'df_tools_frontend');
+
+    $(window).bind('beforeunload', function (e) {
+      if (Drupal.panels_ipe.app.get('unsaved')) {
+        return Drupal.t('You have unchanged changes, are you sure you want to leave?');
+      }
+    });
+  });
+
+}(jQuery, Drupal, Backbone));
