36e5e61f8cf42f1b30e41ec3bc8e5ab554dba8f9
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-01-09 11:24:27 -0800

    Close the moderation sidebar when panels ipe is open.

--- modules/df/df_tools/df_tools_frontend/js/panels_ipe.js
+++ modules/df/df_tools/df_tools_frontend/js/panels_ipe.js
@@ -13,6 +13,15 @@
       location.reload();
     }, 'df_tools_frontend');
 
+    Drupal.panels_ipe.app.get('tabCollection').on('change', function (tab) {
+      if (tab.get('active')) {
+        var $offcanvas = $('#drupal-offcanvas');
+        if ($offcanvas.length && $offcanvas.dialog) {
+          $offcanvas.dialog('close');
+        }
+      }
+    }, 'df_tools_frontend');
+
     $(window).bind('beforeunload', function (e) {
       if (Drupal.panels_ipe.app.get('unsaved') && !Drupal.panels_ipe.app.get('cancelTab').get('loading')) {
         return Drupal.t('You have unchanged changes, are you sure you want to leave?');
