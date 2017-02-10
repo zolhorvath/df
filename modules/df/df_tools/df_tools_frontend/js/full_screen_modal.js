3c2701c57578f4ca75b3d9bf81987cca1503bac8
Brian Wald <briancwald@gmail.com>
2017-02-10 13:49:39 -0500

    modify full width modal for more consistant behavior, update styling

--- modules/df/df_tools/df_tools_frontend/js/full_screen_modal.js
+++ modules/df/df_tools/df_tools_frontend/js/full_screen_modal.js
@@ -4,26 +4,33 @@
     $(window).on({
       'dialog:aftercreate': function (event, dialog, $modal, settings) {
         var $child = $modal.find('iframe');
+        var fWidth = window.innerWidth-20;
+        var fHeight = window.innerHeight-20;
         if ($child.length > 0) {
-          if ($child.attr('src').indexOf('/entity-browser/modal/browse_content') == 0 ||
-            $child.attr('src').indexOf('/entity-browser/modal/image_browser') == 0 ||
-            $child.attr('src').indexOf('/entity-browser/iframe/media_browser') == 0 ||
-            $child.attr('src').indexOf('/entity-browser/modal/media_browser_in_modal') == 0) {
+           var modalSrc = $child.attr('src');
+           var splits = modalSrc.split('/');
+          if (splits[1] == 'entity-browser') {
             // Make the modal full width.
             $modal.dialog({
-              width: '100vw',
-              height: $(window).height(),
-              close: function () {
+              resizable: false,
+              height: fHeight,
+              width: fWidth,
+              position: {
+              my: 'center center',
+              at: 'center center',
+              of: window,
+              collision: 'none'
+              },
+              close: function (event,ui) {
                 $('body').removeClass('full-width-modal-open').css('overflow','auto');
-              }
+              },
             });
-            $modal.parent().addClass('ui-dialog-full-width');
-            $child.css('height', $modal.innerHeight());
-            $('body').addClass('full-width-modal-open');
+              $modal.parent().addClass('ui-dialog-full-width');
+              $child.css('height', $modal.innerHeight());
+              $('body').addClass('full-width-modal-open');
           }
         }
       }
     });
   });
-
 })(jQuery, Drupal);
