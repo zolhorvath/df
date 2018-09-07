ef4ca94d1c360857c33518761aaef5d2842e5591
Brant Wynn <brant.wynn@acquia.com>
2018-09-07 13:05:39 -0500

    Updated the Site Info resource to return the logo information via REST.

--- modules/df/df_tools/df_tools_services/src/Plugin/rest/resource/SiteInfoResource.php
+++ modules/df/df_tools/df_tools_services/src/Plugin/rest/resource/SiteInfoResource.php
@@ -21,15 +21,17 @@ class SiteInfoResource extends ResourceBase {
   /**
    * Responds to GET requests.
    *
-   * @return \Drupal\rest\ResourceResponse
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function get() {
     $theme = \Drupal::config('system.theme')->get('default');
     $site_name = \Drupal::config('system.site')->get('name');
+    $logo = theme_get_setting('logo', $theme);
     $theme_palette = color_get_palette($theme);
     $response = [
       'site_name' => $site_name,
       'theme_palette' => is_array($theme_palette) ? $theme_palette : [],
+      'logo' => is_array($logo) ? $logo : [],
     ];
     return new JsonResponse($response);
   }
