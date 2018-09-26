31810ef0de0704c66c7eed0214ae825590856e07
Kevin Funk <kevin.funk@acquia.com>
2018-09-26 11:37:55 -0400

    Add color preview for body text, links, button and button action.

--- themes/dfs_admin/color/preview.js
+++ themes/dfs_admin/color/preview.js
@@ -26,6 +26,44 @@
 
       $('.color-preview .breadcrumb-nav ol li span').css('color', $colorPalette.find('input[name="palette[breadcrumbtitle]"]').val());
 
+      $('.color-preview .body').css('background-color', $colorPalette.find('input[name="palette[base]"]').val());
+
+      $('.color-preview .btn').css('background-color', $colorPalette.find('input[name="palette[button]"]').val());
+
+      $('.color-preview .btn').css('color', $colorPalette.find('input[name="palette[buttontext]"]').val());
+
+      $('.color-preview .btn').hover(function(){
+          $(this).css('color', $colorPalette.find('input[name="palette[buttonhovertext]"]').val());
+      }, function(){
+          $(this).css('color', $colorPalette.find('input[name="palette[buttontext]"]').val());
+      });
+
+      $('.color-preview .btn').hover(function(){
+          $(this).css('background-color', $colorPalette.find('input[name="palette[buttonhover]"]').val());
+      }, function(){
+          $(this).css('background-color', $colorPalette.find('input[name="palette[button]"]').val());
+      });
+
+      $('.color-preview .button-action').css('background-color', $colorPalette.find('input[name="palette[buttonaction]"]').val());
+
+      $('.color-preview .button-action').each(function () {
+        this.style.setProperty( 'color', $colorPalette.find('input[name="palette[buttonactiontext]"]').val(), 'important' );
+      });
+
+      $('.color-preview .button-action').hover(function(){
+          this.style.setProperty( 'color', $colorPalette.find('input[name="palette[buttonactionhovertext]"]').val(), 'important' );
+      }, function(){
+          this.style.setProperty( 'color', $colorPalette.find('input[name="palette[buttonactiontext]"]').val(), 'important' );
+      });
+
+      $('.color-preview .button-action').hover(function(){
+          $(this).css('background-color', $colorPalette.find('input[name="palette[buttonactionhover]"]').val());
+      }, function(){
+          $(this).css('background-color', $colorPalette.find('input[name="palette[buttonaction]"]').val());
+      });
+
+
+
     }
   };
 })(jQuery, Drupal, drupalSettings);
