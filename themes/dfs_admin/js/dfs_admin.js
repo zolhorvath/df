61318a5006c126e1b2e5dbc22d6d5e8551e12c39
Kevin Funk <kevin.funk@acquia.com>
2018-11-29 14:30:42 -0500

    Renders color preview in a modal

--- themes/dfs_admin/js/dfs_admin.js
+++ themes/dfs_admin/js/dfs_admin.js
@@ -8,14 +8,29 @@
 
   Drupal.behaviors.DFSAdmin = {
     attach: function (context, settings) {
-      // Turn color preview h2 into a button
-      $('.color-form').find('h2').replaceWith(function() {
-        return '<a class="btn color-preview-preview">' + $(this).text() + '</a>';
-      });
 
-      // Toggle color preview display
-      $('a.btn.color-preview-preview').click(function() {
-         $('.color-preview').toggle();
+      $(function dfsModal () {
+         // Turn color preview h2 into a button
+        $('.color-form').find('h2').replaceWith(function() {
+          return '<a class="btn color-preview-preview">' + $(this).text() + '</a>';
+        });
+
+        var mWidth = $(window).width()-20;
+        var mHeight = $(window).height()-20;
+
+        $('.color-preview').dialog({
+          autoOpen: false,
+          title: 'Color Preview',
+          width: mWidth,
+          maxHeight: mHeight,
+          modal: true,
+          draggable: false,
+          resizable: false,
+        });
+
+        $('.color-preview-preview').on('click', function() {
+          $('.color-preview').dialog('open');
+        });
       });
     }
   }
