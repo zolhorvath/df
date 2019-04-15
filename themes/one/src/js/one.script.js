10fb4eb17adc8310f9032b868fa03da910909eba
Kevin Funk <kevin.funk@acquia.com>
2019-04-15 13:51:22 -0400

    Updates meta header to work with menus

--- themes/one/src/js/one.script.js
+++ themes/one/src/js/one.script.js
@@ -6,25 +6,24 @@ import 'bootstrap';
 
   Drupal.behaviors.navbarHeight = {
     attach: function attach(context, settings) {
-
       // Make sure the header has the correct height.
       function navbarHeight() {
         if (width.matches) {
           // Find tallest item and set the value to equalHeights
-          var equalHeights = Math.max($('.navbar .navbar-brand').height(), +
-            $('.navbar .navbar-collapse').height(), +
-            $('.navbar .navbar-right').height());
+          var equalHeights = Math.max($('.page__navbar .navbar-brand').height(), +
+            $('.page__navbar .navbar-collapse').height(), +
+            $('.page__navbar .navbar-right').height());
 
           // Set each div to equalHeights.
           // I'm not sure why I need to subtrack on navbar-brand and navbar-right.
-          $('.navbar .navbar-brand').height(equalHeights - 10);
-          $('.navbar .navbar-collapse').height(equalHeights);
-          $('.navbar .navbar-right').height(equalHeights - 16);
+          $('.page__navbar .navbar-brand').height(equalHeights - 10);
+          $('.page__navbar .navbar-collapse').height(equalHeights);
+          $('.page__navbar .navbar-right').height(equalHeights - 16);
         } else {
           // Remove height
-          $('.navbar .navbar-brand').height('');
-          $('.navbar .navbar-collapse').height('');
-          $('.navbar .navbar-right').height('');
+          $('.page__navbar .navbar-brand').height('');
+          $('.page__navbar .navbar-collapse').height('');
+          $('.page__navbar .navbar-right').height('');
         }
       }
       // Only apply equalHeights on min-width of 992px
