67e8ea495ae32ba17e5db25c4d68434ff6cf80b4
Brant Wynn <brant.wynn@acquia.com>
2019-06-07 12:40:53 -0500

    Adds a preview image from source.unsplash.com when using the field

--- modules/df/df_tools/df_tools_media/js/df_tools_media.js
+++ modules/df/df_tools/df_tools_media/js/df_tools_media.js
@@ -1,33 +1,41 @@
 /**
  * @file df_tools_media.js
+ * Adds some UX improvements for working with Unsplash on Media Image entities.
  */
 (function ($, Drupal) {
-
   "use strict";
-
-  /**
-   * Adds some UX improvements for working with Unsplash on Media.
-   */
+  // Finds expected fields that come via turning this module on.
+  var unsplash = $('.field--name-field-unsplash-image-id input');
+  var image = $('.field--name-image');
+  // Standard behaviors fire on page load once.
   Drupal.behaviors.DFToolsMediaUnsplashFields = {
     attach: function (context) {
-      // Finds expected fields that come via turning this module on.
-      var unsplash = $('.field--name-field-unsplash-image-id input');
-      var image = $('.field--name-image');
+      // Initial setup for when user is editing existing media.
+      image.after('<sub>Note: Image uploading is disabled when Unsplash Image ID is present.</sub>');
+      // Attaches a function to the Unsplash Image ID field for our show/hide.
       unsplash.blur(function() {
         if ( $(this).val() ) {
           image.hide();
+          loadUnsplashPreview($(this).val());
         }
         else {
           image.show();
+          removeUnsplashPreview();
         }
       });
-      // Setup for when editing.
-      image.after('<sub>Note: Image uploading is disabled when Unsplash image ID is present.</sub>');
-      // Slide the image out to prevent too much FOUT.
+      // On load, check for Unsplash value and slide the image out.
       if ( unsplash.val() ) {
         image.slideUp(900);
       }
     }
   };
-
+  // Hit the source.unsplash.com api and produce an image preview.
+  function loadUnsplashPreview(slug) {
+    removeUnsplashPreview();
+    unsplash.after('<p class="unsplash-image-preview"><img src="http://source.unsplash.com/' + slug + '" width="300"/><p>');
+  }
+  // Removes the unsplash image preview from the DOM.
+  function removeUnsplashPreview() {
+    $('.unsplash-image-preview').remove();
+  }
 }(jQuery, Drupal));
