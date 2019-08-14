db7bbf8d272718b9272007931c03c0ceccd6b142
Kevin Funk <kevin.funk@acquia.com>
2019-08-14 17:48:53 -0400

    Adds Material Admin styles for Layout Builder Modal.

--- themes/one/assets/js/one.script.js
+++ themes/one/assets/js/one.script.js
@@ -22630,28 +22630,41 @@ __webpack_require__.r(__webpack_exports__);
       width.addListener(navbarHeight);
     }
   };
-  Drupal.behaviors.updateLBM = {
-    attach: function attach() {
-      $(document).ready(function () {
-        $(window).once('updateLBM').on('dialog:aftercreate', function () {
-          if ($('#layout-builder-modal').length) {
-            $('#layout-builder-modal').dialog({
-              draggable: true
-            });
-            $('#layout-builder-modal').parent().addClass('lbm').css('z-index', '1201');
-          }
-        });
+  Drupal.behaviors.layoutBuilderModal = {
+    attach: function attach(context, settings) {
+      var lbm = $('#layout-builder-modal');
+      var lbmTextInputs = $('#layout-builder-modal input[type="text"]'); // Add active class to label so we can change it's position.
+
+      lbmTextInputs.parent().addClass('input-field');
+      lbmTextInputs.focus(function () {
+        $(this).parent().find("label").addClass("active");
       });
-    }
-  };
-  Drupal.behaviors.closeLBM = {
-    attach: function attach() {
+      lbmTextInputs.focusout(function () {
+        if (!$(this).val()) {
+          $(this).parent().find("label").removeClass("active");
+        }
+      });
+      lbmTextInputs.each(function () {
+        if ($(this).val()) {
+          $(this).parent().find("label").addClass("active");
+        }
+      });
       $(document).ready(function () {
-        if ($('#layout-builder-modal').dialog('isOpen') === true) {
+        // Close modal if clicked outside.
+        if (lbm.dialog('isOpen') === true) {
           $('.ui-widget-overlay').bind('click', function () {
-            $('#layout-builder-modal').dialog('close');
+            lbm.dialog('close');
           });
         }
+
+        $(window).once('layoutBuilderModal').on('dialog:aftercreate', function () {
+          if (lbm.length) {
+            lbm.dialog({
+              draggable: true
+            });
+            lbm.parent().addClass('lbm').css('z-index', '1201');
+          }
+        });
       });
     }
   };
--- themes/one/src/js/one.script.js
+++ themes/one/src/js/one.script.js
@@ -34,29 +34,42 @@ import '@fortawesome/fontawesome-free/js/all';
     }
   };
 
-  Drupal.behaviors.updateLBM = {
-    attach: function() {
-      $(document).ready(function () {
-        $(window).once('updateLBM').on('dialog:aftercreate', function () {
-          if ($('#layout-builder-modal').length) {
-            $('#layout-builder-modal').dialog({
-              draggable: true,
-            });
-            $('#layout-builder-modal').parent().addClass('lbm').css('z-index','1201');
-          }
-        });
+  Drupal.behaviors.layoutBuilderModal = {
+    attach: function attach(context, settings) {
+      var lbm = $('#layout-builder-modal');
+      var lbmTextInputs = $('#layout-builder-modal input[type="text"]');
+      // Add active class to label so we can change it's position.
+      lbmTextInputs.parent().addClass('input-field');
+      lbmTextInputs.focus(function() {
+        $(this).parent().find("label").addClass("active");
       });
-    }
-  };
+      lbmTextInputs.focusout(function() {
+        if (!$(this).val()){
+          $( this ).parent().find( "label").removeClass("active");
+        }
+      });
+      lbmTextInputs.each(function(){
+        if ($(this).val()){
+          $(this).parent().find("label").addClass("active");
+        }
+      });
 
-  Drupal.behaviors.closeLBM = {
-    attach: function() {
       $(document).ready(function () {
-        if ($('#layout-builder-modal').dialog('isOpen') === true) {
+        // Close modal if clicked outside.
+        if (lbm.dialog('isOpen') === true) {
           $('.ui-widget-overlay').bind('click', function () {
-            $('#layout-builder-modal').dialog('close');
+            lbm.dialog('close');
           })
         }
+
+        $(window).once('layoutBuilderModal').on('dialog:aftercreate', function () {
+          if (lbm.length) {
+            lbm.dialog({
+              draggable: true,
+            });
+            lbm.parent().addClass('lbm').css('z-index','1201');
+          }
+        });
       });
     }
   };
